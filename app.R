56bc69b5e9162470be3541339173cfd3b427ef0d
waseem <waseemmedhatg@gmail.com>
2020-05-01 17:23:12 +0200

    moved title argument to header

--- app.R
+++ app.R
@@ -1,13 +1,15 @@
 library(shiny)
 library(shinydashboard)
 
-dbHeader <- dashboardHeader()
+dbHeader <- dashboardHeader(
+  title = "A Mean and a Median"
+)
 
 dbSidebar <- dashboardSidebar()
 
 dbBody <- dashboardBody()
 
-ui <- dashboardPage(dbHeader, dbSidebar, dbBody, "A Mean and a Median", "black")
+ui <- dashboardPage(dbHeader, dbSidebar, dbBody, skin = "black")
 
 server <- function(input, output) {}
 
