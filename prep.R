4e6c1210f0fb15b0e087679a5401d79010390bb4
waseem <waseemmedhatg@gmail.com>
2020-05-01 22:48:16 +0200

    fixed prep to source dotplot.R

--- prep.R
+++ prep.R
@@ -1,7 +1,7 @@
 set.seed(15)
 theme_set(theme_minimal(base_size = 16))
 
-source("histogram.R")
+source("dotplot.R")
 
 dplot1 <- reactive({ histogram(rnorm(100)) })
 dplot2 <- reactive({ histogram(rchisq(100, 2)) })
