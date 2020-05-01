148eb683bcfd4c8ab9c939641eaf17be89d66807
waseem <waseemmedhatg@gmail.com>
2020-05-01 18:22:25 +0200

    added layout of 3 plots in prepared plots tab

--- app.R
+++ app.R
@@ -1,5 +1,12 @@
 library(shiny)
 library(shinydashboard)
+library(ggplot2)
+
+theme_set(theme_minimal(base_size = 16))
+
+dplot1 <- reactive({ ggplot(mtcars, aes(x = mpg)) + geom_dotplot() })
+dplot2 <- reactive({ ggplot(mtcars, aes(x = wt)) + geom_dotplot() })
+dplot3 <- reactive({ ggplot(mtcars, aes(x = disp)) + geom_dotplot() })
 
 dbHeader <- dashboardHeader(
   title = "A Mean and a Median"
@@ -23,7 +30,14 @@ dbBody <- dashboardBody(
         subtitle = "Look at these plots, which are pre-made for you.",
         color = "green",
         width = 12
-      )
+      ),
+      
+      fluidRow(
+        column(plotOutput('dplot1'), width = 6),
+        column(plotOutput('dplot2'), width = 6)
+      ),
+      
+      fluidRow( plotOutput('dplot3') )
       
     ),
     
@@ -34,6 +48,10 @@ dbBody <- dashboardBody(
 
 ui <- dashboardPage(dbHeader, dbSidebar, dbBody, skin = "black")
 
-server <- function(input, output) {}
+server <- function(input, output) {
+  output$dplot1 <- renderPlot({ dplot1() })
+  output$dplot2 <- renderPlot({ dplot2() })
+  output$dplot3 <- renderPlot({ dplot3() })
+}
 
 shinyApp(ui = ui, server = server)
