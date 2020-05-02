3b6009ebf12c703b19cac53232e1d6679bd4fbf0
waseem <waseemmedhatg@gmail.com>
2020-05-02 15:01:33 +0200

    moved generated data outside of reactive expression

--- prep.R
+++ prep.R
@@ -3,7 +3,12 @@ theme_set(theme_minimal(base_size = 16))
 
 source("dotplot.R")
 
-dplot1 <- reactive({ dotplot(rnorm(50)) })
-dplot2 <- reactive({ dotplot(c(rnorm(50), rnorm(50, 5))) })
-dplot3 <- reactive({ dotplot(rchisq(50, 1)) })
-dplot4 <- reactive({ dotplot(-rchisq(50, 1)) })
+d1 <- rnorm(50)
+d2 <- c(rnorm(50), rnorm(50, 5))
+d3 <- rchisq(50, 1)
+d4 <- -rchisq(50, 1)
+
+dplot1 <- reactive({ dotplot(d1) })
+dplot2 <- reactive({ dotplot(d2) })
+dplot3 <- reactive({ dotplot(d3) })
+dplot4 <- reactive({ dotplot(d4) })
